281114affb37914a3d074f65734a3c441a242240
Wei Fu <fuweid89@gmail.com>
2019-01-23 14:30:54 +0800

    scripts: update runc version
    since we run CI with develop branch of alibaba/runc, the runc version is
    changing every day. but we need to use fix commit during the packaging.
    
    therefore, for the v1.2.0 pouchcontainer, we use the specific commit in
    the script. in the following pr, we can use env to modify the commit so
    that we don't need to change it every time.
    
    Signed-off-by: Wei Fu <fuweid89@gmail.com>

--- scripts/download_runc.sh
+++ scripts/download_runc.sh
@@ -2,24 +2,20 @@
 
 set -euo pipefail
 
-readonly RUNC_VERSION="v1.0.0-rc4-2"
+readonly RUNC_VERSION="6a93df143f66a1c8bc967200473a568a8487d287"
 
 # main downloads runc binary into specific dir.
 main() {
-  local url target tmpdir dist
+  local gopath dist
 
   dist="${1}"
+  gopath="${GOPATH}/src/github.com/opencontainers/runc"
 
-  target="runc.amd64"
-  url="https://github.com/alibaba/runc/releases/download/"
-  url="${url}/${RUNC_VERSION}/${target}"
-
-  tmpdir="$(mktemp -d /tmp/runc-download-XXXXXX)"
-  trap 'rm -rf /tmp/runc-download-*' EXIT
-
-  wget --quiet "${url}" -P "${tmpdir}"
-  chmod +x "${tmpdir}/${target}"
-  cp -f "${tmpdir}/${target}" "${dist}/runc"
+  git clone https://github.com/alibaba/runc.git "${gopath}"
+  cd "${gopath}"
+  git checkout "${RUNC_VERSION}"
+  make
+  cp -f runc "${dist}/runc"
 }
 
 main "$@"
